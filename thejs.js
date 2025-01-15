b907fd40dd90bee71d499f0d835ea0eff5d34656
JO-U <147983158+JO-U@users.noreply.github.com>
2025-01-15 22:34:48 +0100

    Add files via upload

---
+++ thejs.js
@@ -0,0 +1,18 @@
+$('.dropdown-toggle').dropdown()
+
+//menu icon
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+
+//
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
